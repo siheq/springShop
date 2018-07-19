e3170eb672d3929af9fffca4895d3e8343610c4f
Simon He <simon.he@cargosmart.com>
2018-07-19 16:37:50 +0800

    update

--- src/main/java/com/simon/shop/controller/UserController.java
+++ src/main/java/com/simon/shop/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.simon.shop.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 //@RequestMapping(value="/api/auth")
 public class UserController {
 //    @GetMapping(value="/login")
     @RequestMapping("/")
-    @ResponseBody
     public String UserLogin () {
         return "Success";
 
